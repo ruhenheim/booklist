d55a80d5f122f19a0e5719e02daebd3673b144d2
ruhenheim <asd.uchiyama@gmail.com>
2017-10-17 06:18:27 +0900

    debug settings

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'mysql2'
 
 set :views, Proc.new { File.join(root, "templates") }
+p settings.views
+p settings.server
 
 get '/' do
   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'booklist', encoding: 'utf8')
